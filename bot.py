3dcd3d6957f478193079f12cd8674171aa17837e
thisisnotjeymz-collab <thisisnotjeymz@gmail.com>
2026-04-09 21:49:29 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,8 +107,8 @@ async def on_message(message):
         await message.reply("bading yan!")
     elif "bobo" in content:
         await message.reply("mas bobo ka")
-    elif "@everyone" in content:
-        await message.reply("nag ping nanaman ang bwakanangina")
+    elif message.mention_everyone:
+        await message.reply("nag ping nanaman ng bwakanangina")
 
     await bot.process_commands(message)
 
